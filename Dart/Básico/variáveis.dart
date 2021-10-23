b795dd305f93c61dc5929dfd1bef06c68fe8468e
danilohbp <danil34681045@gmail.com>
2021-10-23 17:25:20 -0300

    first commit

---
+++ Dart/Básico/variáveis.dart
@@ -0,0 +1,15 @@
+void main() {
+  var livro = 'Guia de Flutter';
+  print(livro);
+  var ano = 2021;
+  print(ano);
+  var decimal = 0.0;
+  print(decimal);
+  var lista = ['Guia', 'de', 'Flutter'];
+  print(lista);
+  var objeto = {
+    'propriedade1': ['Altura', 'Peso'],
+    'propriedade2': 'Guia de Flutter'
+  };
+  print(objeto);
+}
