d9a62756ce25d821092844d5e1a565d83ab8bef4
danilohbp <danil34681045@gmail.com>
2021-10-23 21:12:54 -0300

    Third Commit

---
+++ Dart/Básico/multipla escolha com switch.dart
@@ -0,0 +1,14 @@
+void main() {
+  var valor = 1;
+  switch (valor){
+    case 1: {
+      print('Ok, o valor é o mesmo!');
+    } break;
+    case 2: {
+        print('Ok, o valor é o mesmo!');
+    } break;
+    default: {
+        print('Não há um valor correspondente!');
+    } break;
+  }
+}
---
+++ Dart/Básico/múltipla escolha com else if.dart
@@ -0,0 +1,10 @@
+void main() {
+	var valor = 1;
+  if (valor > 1) {
+    print('Valor é maior do que 1');
+  } else if (valor < 0) {
+    print('Valor é menor do que zero');
+  } else if (valor == 1) {
+    print('Valor é igual a 1');
+  }
+}
